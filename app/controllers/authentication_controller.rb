85dd6e2df6dc9dc2ac20e57fc4b15880ac9e1aec
Faraz Noor <faraznoor@Farazs-MacBook-Pro.local>
2018-05-18 00:52:09 +0530

    Added a service user model which inherits from user

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,8 +1,12 @@
 class AuthenticationController < ApiController
   def authenticate_user
-    user = User.find_for_database_authentication(email: params[:email])
+    user = ServiceUser.find_for_database_authentication(email: params[:email])
     if user.valid_password?(params[:password])
-      render json: payload(user)
+      if user.roles=='service'
+        render json: payload(user)
+      else
+        render json: {error:'You not authorized to use services,Kindly ask Admin to eleivate you.'},status: :unauthorized
+      end
     else
       render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
     end
---
+++ app/models/service_user.rb
@@ -0,0 +1,2 @@
+class ServiceUser < User
+end
